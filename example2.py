7f617541d41496e18397ea5d0ce57f6058ec3a56
nuwanthikaishani <nuwanthikaishani@gmail.com>
2026-01-05 18:42:45 +0530

    create new example file

---
+++ example2.py
@@ -0,0 +1,18 @@
+#Scenario : Shopping list
+# Create an empty list
+ShoppingList = ["Watch", "Laptop", "Shoes", "Pen", "Clothes"]
+print(ShoppingList)
+print(ShoppingList[2])
+#Add a new item to the shopping_list
+ShoppingList.append("Football")
+print(ShoppingList)
+print(ShoppingList[5])
+#Print First item from the shopping_list
+print(ShoppingList[0])
+#Change the item from the shopping_list
+print(ShoppingList[3])
+ShoppingList[3] = "Notebook"
+print(ShoppingList[3])
+print(ShoppingList)
+del (ShoppingList[4])
+print(ShoppingList)
